afc57015df03fc009d673bde5cb06cdf017c6a13
sonnned <alejo1garciasosa@gmail.com>
2023-10-29 15:17:25 -0500

    APP Macros

---
+++ macros.h
@@ -0,0 +1,11 @@
+#ifndef MACROS_H
+#define MACROS_H
+
+#define BOARD_HEIGHT 8
+#define BOARD_WEIGHT 8
+#define BOARD_SPACING '\t'
+#define BOARD_EMPTY_SPACE ' '
+#define WHITE '*'
+#define BLACK '-'
+
+#endif // MACROS_H
