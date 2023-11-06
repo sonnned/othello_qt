0bb66416f40de51d47fb8c5cd02e5d1fc0ee0dcb
sergio-alarcon1 <sergio.alarcon1@udea.edu.co>
2023-11-06 18:47:12 -0500

    include

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include "matriz.h"
 #include "macros.h"
+#include <limits>
+
+
+
 //Para BOARD_SIZE = 4
 //(2,D) (3,D) (4,D) (1,D) (3,A) (4,A) (1,C) (1,B) (2,A) (1,A) (4,B) (4,C)
 int main()
