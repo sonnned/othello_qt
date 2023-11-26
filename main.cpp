5cd6cf553e78b9117d2432c7edb41d1d8e0ccb92
sergio-alarcon1 <sergio.alarcon1@udea.edu.co>
2023-11-26 18:34:10 -0500

    asdfg

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include "macros.h"
 #include "game.h"
 #include "matriz.h"
-
 #include <iostream>
+#include <limits>
 
 //Para BOARD_SIZE = 4
 //(2,D) (3,D) (4,D) (1,D) (3,A) (4,A) (1,C) (1,B) (2,A) (1,A) (4,B) (4,C)
